62fff1e9c7761c5c50377e9fcaffa6f2b24931ac
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-01-18 17:17:06 +0100

    liquidity responvinsess

--- src/components/liquidity/Liquidity.jsx
+++ src/components/liquidity/Liquidity.jsx
@@ -6,24 +6,26 @@ import './liquidity.css'
 
 function Liquidity() {
   return (
-    <div className='liquidity-wrapper'>
-      <HeaderSwap
-        content={{
-          title: 'Your Liquidity',
-          subtitle: 'Remove liquidity to receive tokens back',
-          class: 'liquidity-header',
-        }}
-      />
-      <div className='liquidity-content'>
-        <p>Connect to a wallet to view your liquidity.</p>
+    <section className='liquidity'>
+      <div className='liquidity-wrapper'>
+        <HeaderSwap
+          content={{
+            title: 'Your Liquidity',
+            subtitle: 'Remove liquidity to receive tokens back',
+            class: 'liquidity-header',
+          }}
+        />
+        <div className='liquidity-content'>
+          <p>Connect to a wallet to view your liquidity.</p>
+        </div>
+        <div className='liquidity-btn'>
+          <button>
+            <PlusIcon className='plus-icon' />
+            Add Liquidity
+          </button>
+        </div>
       </div>
-      <div className='liquidity-btn'>
-        <button>
-          <PlusIcon  className='plus-icon'/>
-          Add Liquidity
-        </button>
-      </div>
-    </div>
+    </section>
   )
 }
 
